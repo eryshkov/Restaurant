c2af2aeea67f2760158eab22bdd98ce013b9250b
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-19 20:10:16 +0300

    views added

---
+++ View Controllers/ItemDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemDetailViewController.swift
+//  Restaurant
+//
+//  Created by Evgeniy Ryshkov on 19/11/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
