0aeea1c678c1b0f152433be1f2f7277378c7bdb8
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-19 21:17:43 +0300

    fetchMenuItems implementation

--- Controllers/Network/MenuController.swift
+++ Controllers/Network/MenuController.swift
@@ -11,4 +11,23 @@ import Foundation
 class MenuController {
     let baseURL = URL(string: "http://api.armenu.net:8090/")!
     
+    func fetchCategories(completion: @escaping ([String]?) -> Void) {
+        let categoryURL = baseURL.appendingPathComponent("categories")
+    }
+    
+    func fetchMenuItems(forCategory categoryName: String,
+                        completion: @escaping ([MenuItem]?) -> Void
+        ) {
+        let initialURL = baseURL.appendingPathComponent("menu")
+        var components = URLComponents(url: initialURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        guard let menuURL = components.url else {fatalError("\(#function) Erroneous category name for GET request")}
+        
+    }
+    
+    func submitOrder(forMenuIDs menuIDs: [Int],
+                     completion: (Int?) -> Void
+        ) {
+        let orderURL = baseURL.appendingPathComponent("order")
+    }
 }
