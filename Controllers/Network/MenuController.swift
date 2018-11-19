b9ef966695cf05c9edf3aa2f13c0fa74c5f78380
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-19 22:20:45 +0300

    updateUI implemented

--- Controllers/Network/MenuController.swift
+++ Controllers/Network/MenuController.swift
@@ -13,9 +13,26 @@ class MenuController {
     
     func fetchCategories(completion: @escaping ([String]?) -> Void) {
         let categoryURL = baseURL.appendingPathComponent("categories")
+        
         let task = URLSession.shared.dataTask(with: categoryURL) {
-            data, responce, error in
-            //TODO: get categories
+            data, response, error in
+            
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            guard let jsonDictionary = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+                completion(nil)
+                return
+            }
+            
+            guard let categories = jsonDictionary?["categories"] as? [String] else{
+                completion(nil)
+                return
+            }
+            
+            completion(categories)
         }
         task.resume()
     }
@@ -28,11 +45,58 @@ class MenuController {
         components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
         guard let menuURL = components.url else {fatalError("\(#function) Erroneous category name for GET request")}
         
+        let task = URLSession.shared.dataTask(with: menuURL) {
+            data, response, error in
+            
+            let jsonDecoder = JSONDecoder()
+            
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            guard let menuItems = try? jsonDecoder.decode(MenuItems.self, from: data) else {
+                completion(nil)
+                return
+            }
+            
+            completion(menuItems.items)
+            
+        }
+        task.resume()
     }
     
     func submitOrder(forMenuIDs menuIDs: [Int],
-                     completion: (Int?) -> Void
+                     completion: @escaping (Int?) -> Void
         ) {
         let orderURL = baseURL.appendingPathComponent("order")
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let data: [String: [Int]] = ["menuIDs": menuIDs]
+        
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(data)
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            
+            let jsonDecoder = JSONDecoder()
+            
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            guard let preparationTime = try? jsonDecoder.decode(PreparationTime.self, from: data) else {
+                completion(nil)
+                return
+            }
+            
+            completion(preparationTime.prepTime)
+        }
+        task.resume()
     }
 }
--- Controllers/View Controllers/CategoryTableViewController.swift
+++ Controllers/View Controllers/CategoryTableViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
+    
+    let menuController = MenuController()
+    var categories = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        menuController.fetchCategories { (categories) in
+            if let categories = categories {
+                self.updateUI(with: categories)
+            }
+        }
+    }
+    
+    func updateUI(with categories: [String]) {
+        self.categories = categories
+        print("\(#function) \(categories)")
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
