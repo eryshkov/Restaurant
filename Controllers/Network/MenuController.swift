44c260368126c4da0e872547006d0faa427a4eb2
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-19 20:50:54 +0300

    MenuController added

---
+++ Controllers/Network/MenuController.swift
@@ -0,0 +1,14 @@
+//
+//  MenuController.swift
+//  Restaurant
+//
+//  Created by Evgeniy Ryshkov on 19/11/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+class MenuController {
+    let baseURL = URL(string: "http://api.armenu.net:8090/")!
+    
+}
